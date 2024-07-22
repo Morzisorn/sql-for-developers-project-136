2539174b5568d07a429bafec82efa369f46e821c
Morzisorn <morzisorc1@gmail.com>
2024-07-22 18:58:12 +0100

    added database.sql and step 1

---
+++ database.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Programs (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    price INT,
+    type VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE Modules (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    program_id BIGINT NOT NULL REFERENCES Programs(id)
+);
+
+
+CREATE TABLE Courses (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE Modules_Courses (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    module_id BIGINT NOT NULL REFERENCES Modules(id),
+    course_id BIGINT NOT NULL REFERENCES Courses(id)
+);
+
+CREATE TABLE Lessons (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    course_id BIGINT REFERENCES Courses(id),
+    name VARCHAR(255),
+    content TEXT,
+    video_link VARCHAR(255),
+    lesson_position INT,
+    is_deleted BOOLEAN,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
